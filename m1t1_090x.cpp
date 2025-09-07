627ca4701e8fc8407dc060ede011f48b5bcfbe33
alex <acevedoa9596@student.faytechcc.edu>
2025-09-07 20:26:37 +0000

    Finished M1T1

--- m1t1_090x.cpp
+++ m1t1_090x.cpp
@@ -5,23 +5,24 @@ acevedoa
 9/07/25    
 Program will greet the user.
 
-If it happens:
+ERROR HANDLING WITH CODESPACES:
+IF it happens:
 "stage changes" , no, push directly
 "commit and sync", always
-"pull?" doesnt matter
+"pull?" doesn't matter
 */
  
 // magic words
 #include <iostream>
 using namespace std;
 
-int main(){
+int main() {
     // input: start the program
     // processing: none
     // output: 0 (no erors)
     // special FX: says "Hello" to the user.
 
-    // let's say hello
+    // Let's say hello
     cout << "Hello, user!" << endl; // "end line" not "end 1"
 
     return 0; // no errors
