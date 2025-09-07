cfba05ea2cede79c7702f623759b6506f09086f6
alex <acevedoa9596@student.faytechcc.edu>
2025-09-07 20:21:19 +0000

    Finished M1T1

--- m1t1_090c.cpp
+++ m1t1_090c.cpp
@@ -4,5 +4,25 @@ M1T1 for the online sections
 acevedoa
 9/07/25    
 Program will greet the user.
-*/
 
+If it happens:
+"stage changes" , no, push directly
+"commit and sync", always
+"pull?" doesnt matter
+*/
+ 
+// magic words
+#include <iostream>
+using namespace std;
+
+int main(){
+    // input: start the program
+    // processing: none
+    // output: 0 (no erors)
+    // special FX: says "Hello" to the user.
+
+    // let's say hello
+    cout << "Hello, user!" << endl; // "end line" not "end 1"
+
+    return 0; // no errors
+} 
