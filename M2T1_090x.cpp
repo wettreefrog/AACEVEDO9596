80e4979c768afe704d48cae1863add9a55599ffe
alex <acevedoa9596@student.faytechcc.edu>
2025-09-21 21:40:06 +0000

    finished

--- M2T1_090x.cpp
+++ M2T1_090x.cpp
@@ -5,12 +5,14 @@
 
   
  #include <iostream> 
+ #include <iomanip> // for the 2 decimal places
  using namespace std; 
   
  int main() { 
     
  string first_name, last_name, full_name; // holds customer name 
  string product = "apples"; // chane to whatever you like
+ int amount_purchased;
  double cost_each = 0.99;
  double total_cost;
 
@@ -22,5 +24,16 @@ cin >> last_name;
 full_name = first_name + " " + last_name;
 cout << "Nice to meet you, " << first_name << endl;
 
+cout << "How many " << product << " would you like today? ";
+cin >> amount_purchased;
+
+total_cost = amount_purchased * cost_each;
+
+// formatting set all prices to 2 decimal places
+cout << setprecision(2) << fixed;
+
+cout << " for " << amount_purchased << " " << product << endl;
+cout << "That will be: $" << total_cost << endl;
+cout << "Thank you for shopping with us, " << full_name << "!" << endl;
 return 0; // no errors 
 }
