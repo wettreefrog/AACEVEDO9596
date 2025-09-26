8ac8debbba28f21163420b30350f727a15a558a0
alex <acevedoa9596@student.faytechcc.edu>
2025-09-26 01:12:11 +0000

    finished M3CW1_Acevedo.java

---
+++ M3CW1_Acevedo.java
@@ -0,0 +1,16 @@
+public class M3CW1_Acevedo  
+{ 
+    public static void main(String[] args)  
+    { 
+       System.out.println("Alexander");  
+       System.out.println("Acevedo");  
+       System.out.println("2025 Fall Schedule");  
+
+       System.out.println("CIS-115-0001 Intro to Programming and Logic");  
+       System.out.println("ENG-111-0003 Writing and Inquiry");  
+       System.out.println("MAT-121-0002 Algebra/Trigonometry I");  
+       System.out.println("COM-231-0001 Public Speaking");  
+
+       System.out.println("Experience: Beginner – some Java and C++ basics from coursework.");  
+    } 
+} 
